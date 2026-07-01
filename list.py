e95fdb8c1fe356a255e2145b5f01dbff680173b4
Dev-Moynul <stu.moynulislam@gmail.com>
2026-07-01 16:52:09 +0600

    new list function add

--- list.py
+++ list.py
@@ -1,17 +1,27 @@
-# input
-#arr = list(map(int,input().split()))
-#print (len(arr))
-# for x in arr:
-#     print(arr)
+input
+arr = list(map(int,input().split()))
+print (len(arr))
+for x in arr:
+    print(arr)
 
-# for i  in range(len(arr)):
-#     print(arr[i])
+for i  in range(len(arr)):
+    print(arr[i])
 
 arrr = [5, 6, 6 ,3, 14, 75]
-#print(arrr, arrr.sort(reverse=True))
+print(arrr, arrr.sort(reverse=True))
 
 print(max(arrr))
 
 arrr[::-1]
 print(arrr)
 print(arrr,arrr.index(6))
+
+
+arr = [4,5,6,7,8,9,10]
+#arr.insert(4,100)
+arr.pop(2)
+print(arr)
+print(arr[:])
+print(arr[::-1])
+arr.sort()
+print(arr)
