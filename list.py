6f81bf8095de2d6d89cab7712740662c3f091239
Dev-Moynul <stu.moynulislam@gmail.com>
2026-07-01 12:37:54 +0600

    Python practice list

---
+++ list.py
@@ -0,0 +1,17 @@
+# input
+#arr = list(map(int,input().split()))
+#print (len(arr))
+# for x in arr:
+#     print(arr)
+
+# for i  in range(len(arr)):
+#     print(arr[i])
+
+arrr = [5, 6, 6 ,3, 14, 75]
+#print(arrr, arrr.sort(reverse=True))
+
+print(max(arrr))
+
+arrr[::-1]
+print(arrr)
+print(arrr,arrr.index(6))
