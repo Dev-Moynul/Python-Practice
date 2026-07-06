3da608f4607b15d90f1e0a853ac2f13e787eaaa4
Dev-Moynul <stu.moynulislam@gmail.com>
2026-07-07 00:16:43 +0600

    update value arr use  enumerate

--- enumirate.py
+++ enumirate.py
@@ -8,3 +8,20 @@ for i in range(len(arr)):
 arr = [10, 5, 30]
 for i, x in enumerate(arr):
     print(i, x)
+
+#Starting Index Change
+arr = [10, 5, 30]
+for i, x in enumerate(arr, start=1):
+    print(i, x)
+
+#Ami jante cai 9 kon index er moddhe ache
+arr = [5,7,9,2]
+for i, x in enumerate(arr):
+    if x == 9:
+        print(i)
+
+#Arr update korte cai
+arr = [5,7,9,2]
+for i, x in enumerate(arr):
+    arr[i] = x * 2
+print(arr)
